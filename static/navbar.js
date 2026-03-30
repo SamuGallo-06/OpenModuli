5054552d2adbc87a17aa08570efeb7b5689727f9
SamuGallo_06 <samu.gallicani@gmail.com>
2026-03-30 22:25:21 +0200

    Refactor admin navigation: update menu item labels and remove exit option; add navbar JavaScript functions

---
+++ static/navbar.js
@@ -0,0 +1,47 @@
+function onNewFormClicked(){
+
+}
+
+function onUploadFormClicked(){
+    
+}
+
+function onSettingsClicked(){
+    
+}
+
+function onReloadConfigurationClicked(){
+    
+}
+
+function onRebootApplicationClicked(){
+    
+}
+
+function onRebootServerClicked(){
+    
+}
+
+function onShutdownServerClicked(){
+    
+}
+
+function onDocumentationClicked(){
+    
+}
+
+function onContactSupportClicked(){
+    
+}
+
+function onGitHubRepoClicked(){
+    
+}
+
+function onLicenceClicked(){
+    
+}
+
+function onAboutClicked(){
+    
+}
